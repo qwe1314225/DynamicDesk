f5f6071b53100641bafbc1328b449cace83e5704
xsusu <zx9797997@163.com>
2017-10-11 16:12:58 +0800

    MainActivity

--- app/src/main/java/com/zxyoyo/hosion/dynamicdesk/MainActivity.java
+++ app/src/main/java/com/zxyoyo/hosion/dynamicdesk/MainActivity.java
@@ -1,13 +1,36 @@
 package com.zxyoyo.hosion.dynamicdesk;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 public class MainActivity extends AppCompatActivity {
+    private CheckBox cbSound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cbSound = (CheckBox) findViewById(R.id.cb_sound);
+        //监听是否开启声音
+        cbSound.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    //开启声音
+                    MyWallpaper.openSound(MainActivity.this);
+                }else {
+                    //关闭声音
+                    MyWallpaper.closeSound(MainActivity.this);
+                }
+            }
+        });
+    }
+
+    //按钮的点击事件
+    public void setDynamicDesk(View view){
+        MyWallpaper.setDynamicWallPaper(this);
     }
 }
